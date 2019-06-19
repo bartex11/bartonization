c2eec60fa9929b4a61aa76e6d59ee0d29fe73fd9
DOM\IvanVasilev <i.vasilev@spicymedia.io>
2019-06-19 16:59:22 +0200

    lightbox

--- src/App.js
+++ src/App.js
@@ -1,163 +1,99 @@
-import React, {Component} from 'react';
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarNav,
-  NavbarToggler,
-  Collapse,
-  NavItem,
-  Footer,
-  NavLink,
-} from 'mdbreact';
-import {HashRouter as Router} from 'react-router-dom';
-import {ThemeProvider} from 'styled-components';
+import React, { Component } from 'react';
+import { NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, Footer, NavLink } from 'mdbreact';
+
+import Navbar from './components/Navbar';
+import { HashRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import './index.scss';
 import theme from './theme';
 import Routes from './Routes';
 
 class App extends Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {
       collapseID: '',
     };
   }
 
-  toggleCollapse = collapseID => () =>
-    this.setState (prevState => ({
+  toggleCollapse = (collapseID) => () =>
+    this.setState((prevState) => ({
       collapseID: prevState.collapseID !== collapseID ? collapseID : '',
     }));
 
-  closeCollapse = collapseID => () =>
-    this.state.collapseID === collapseID && this.setState ({collapseID: ''});
+  closeCollapse = (collapseID) => () => this.state.collapseID === collapseID && this.setState({ collapseID: '' });
 
-  render () {
-    const overlay = (
-      <div
-        id="sidenav-overlay"
-        style={{backgroundColor: 'transparent'}}
-        onClick={this.toggleCollapse ('mainNavbarCollapse')}
-      />
-    );
+  render() {
+    const overlay = <div id="sidenav-overlay" style={{ backgroundColor: 'transparent' }} onClick={this.toggleCollapse('mainNavbarCollapse')} />;
     return (
       <Router>
         <div className="flyout">
           <Navbar color="indigo" dark expand="lg" fixed="top" double scrolling>
             <NavbarBrand href="/">
-              <img
-                src="/images/bart-simpson-picture-png-image-723.png"
-                alt=""
-                height="40"
-                style={{marginRight: '10px'}}
-              />
-              {' '}
-              Bartex React
+              <img src="/images/bart-simpson-picture-png-image-723.png" alt="" height="40" style={{ marginRight: '10px' }} /> Bartex React
             </NavbarBrand>
-            <NavbarToggler
-              onClick={this.toggleCollapse ('mainNavbarCollapse')}
-            />
-            <Collapse
-              id="mainNavbarCollapse"
-              isOpen={this.state.collapseID}
-              navbar
-            >
+            <NavbarToggler onClick={this.toggleCollapse('mainNavbarCollapse')} />
+            <Collapse id="mainNavbarCollapse" isOpen={this.state.collapseID} navbar>
               <NavbarNav>
                 <NavItem>
-                  <NavLink
-                    exact
-                    to="/"
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                  >
+                  <NavLink exact to="/" onClick={this.closeCollapse('mainNavbarCollapse')}>
                     Home
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/test"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/test">
                     Test
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/tabs"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/tabs">
                     Tabs
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/css"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/css">
                     CSS
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/components"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/components">
                     Components
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/advanced"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/advanced">
                     Advanced
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/navigation"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/navigation">
                     Navigation
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/forms"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/forms">
                     Forms
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/tables"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/tables">
                     Tables
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/modals"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/modals">
                     Modals
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/addons"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/addons">
                     Addons
                   </NavLink>
                 </NavItem>
 
                 <NavItem>
-                  <NavLink
-                    onClick={this.closeCollapse ('mainNavbarCollapse')}
-                    to="/background"
-                  >
+                  <NavLink onClick={this.closeCollapse('mainNavbarCollapse')} to="/background">
                     BgImage
                   </NavLink>
                 </NavItem>
@@ -173,19 +109,9 @@ class App extends Component {
           <Footer>
             <p className="footer-copyright mb-0 py-3 text-center">
               &copy; 2016 Copyright:{' '}
-              <a
-                href="http://www.bartonization.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="/images/bart-simpson-picture-png-image-723.png"
-                  alt=""
-                  height="50"
-                  style={{marginRight: '10px'}}
-                />
-                Bartonization
-                {' '}
+              <a href="http://www.bartonization.com/" target="_blank" rel="noopener noreferrer">
+                <img src="/images/bart-simpson-picture-png-image-723.png" alt="" height="50" style={{ marginRight: '10px' }} />
+                Bartonization{' '}
               </a>
             </p>
           </Footer>
--- src/pages_new/TestPage.js
+++ src/pages_new/TestPage.js
@@ -21,10 +21,67 @@ import {
   MDBMask,
   Animation,
   Iframe,
+  MDBRow,
+  MDBCol,
+  MDBBtn,
 } from 'mdbreact';
 
-class TestPage extends Component {
+import './Lightbox.css';
+import Lightbox from 'react-image-lightbox';
+
+class TestPage extends React.Component {
+  state = {
+    photoIndex: 0,
+    isOpen: false,
+    images: [
+      'https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(135).jpg',
+      'https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(128).jpg',
+      'https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(133).jpg',
+      'https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(131).jpg',
+      'https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(130).jpg',
+      'https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(132).jpg',
+      'https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(134).jpg',
+      'https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(1).jpg',
+    ],
+    name: [
+      'Projekt 1',
+      'Projekt 2',
+      'Projekt 3',
+      'Projekt 4',
+      'Projekt 5',
+      'Projekt 6',
+      'Projekt 7',
+      'Projekt 8',
+    ],
+  };
+  renderImages = () => {
+    let photoIndex = -1;
+    const {images} = this.state;
+    const {name} = this.state;
+
+    return images.map (imageSrc => {
+      photoIndex++;
+      const privateKey = photoIndex;
+      return (
+        <MDBCol md="3" key={photoIndex}>
+          <figure>
+            <img
+              src={imageSrc}
+              alt="Gallery"
+              className="img-fluid z-depth-1"
+              onClick={() =>
+                this.setState ({photoIndex: privateKey, isOpen: true})}
+            />
+            <p class="text-uppercase font-weight-bold blue-grey-text mt-4">
+              {name[photoIndex]}
+            </p>
+          </figure>
+        </MDBCol>
+      );
+    });
+  };
   render () {
+    const {photoIndex, isOpen, images} = this.state;
     return (
       <div>
         <EdgeHeader color="indigo darken-3" />
@@ -122,6 +179,48 @@ class TestPage extends Component {
             </Row>
           </Col>
 
+        </Container>
+
+        <Container
+          className="mt-5 p-3"
+          style={{backgroundColor: '#fff', marginBottom: '50px'}}
+        >
+          <h2 class="h1 text-center text-uppercase font-weight-bold pt-5 mb-3 mt-5">
+            My projects
+          </h2>
+          <p class="text-center text-uppercase font-weight-bold">
+            All about my work
+          </p>
+          <p class="section-description mt-5 pt-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error
+            amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam eum porro a
+            pariatur accusamus veniam. At ab ea a molestiae corrupti numquam quo beatae minima ratione magni accusantium
+            repellat eveniet quia vitae.
+          </p>
+          <div className="mdb-lightbox p-3">
+            <MDBRow>
+              {this.renderImages ()}
+            </MDBRow>
+          </div>
+          {isOpen &&
+            <Lightbox
+              mainSrc={images[photoIndex]}
+              nextSrc={images[(photoIndex + 1) % images.length]}
+              prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+              imageTitle={photoIndex + 1 + '/' + images.length}
+              onCloseRequest={() => this.setState ({isOpen: false})}
+              onMovePrevRequest={() =>
+                this.setState ({
+                  photoIndex: (photoIndex + images.length - 1) % images.length,
+                })}
+              onMoveNextRequest={() =>
+                this.setState ({
+                  photoIndex: (photoIndex + 1) % images.length,
+                })}
+            />}
+          <MDBCol md="12" className="text-center py-4">
+            <MDBBtn outline color="black"><strong>more</strong></MDBBtn>
+          </MDBCol>
         </Container>
       </div>
     );
