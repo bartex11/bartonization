980e594b2b22494b8c5a24a2c982799f346279fe
DOM\IvanVasilev <i.vasilev@spicymedia.io>
2019-06-27 13:36:32 +0200

    duss

--- src/pages/MyStory.js
+++ src/pages/MyStory.js
@@ -1,57 +1,49 @@
 import React from 'react';
-import {withNamespaces} from 'react-i18next';
+import { withNamespaces } from 'react-i18next';
 
 import './MyStory.css';
 
 class MyStory extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     document.title = 'My Story';
   }
-  render () {
-    const {t} = this.props;
+  render() {
+    const { t } = this.props;
     return (
       <div>
         <div className="paper-background">
           <span className="coffee-stain" />
-          <span className="paper-date">
-            Düsseldorf, 21.06.2019
-          </span>
+          <span className="duss" />
+          <span className="paper-date">Düsseldorf, 21.06.2019</span>
           <section className="paper-content">
-            <p>
-              {t ('Story')}
-            </p><br />
-            <p>
-              {t ('Story1')}
-            </p><br />
-            <p>
-              {t ('Story2')}
-            </p><br />
-            <p>
-              {t ('Story3')}
-            </p><br /><br />
+            <p>{t('Story')}</p>
+            <br />
+            <p>{t('Story1')}</p>
+            <br />
+            <p>{t('Story2')}</p>
+            <br />
+            <p>{t('Story3')}</p>
+            <br />
+            <br />
             <p class="paper-signature">Ivan Vasilev</p>
           </section>
         </div>
 
         <div class="my-story">
-          <p>
-            {t ('Story')}
-          </p><br />
-          <p>
-            {t ('Story1')}
-          </p><br />
-          <p>
-            {t ('Story2')}
-          </p><br />
-          <p>
-            {t ('Story3')}
-          </p><br /><br />
+          <p>{t('Story')}</p>
+          <br />
+          <p>{t('Story1')}</p>
+          <br />
+          <p>{t('Story2')}</p>
+          <br />
+          <p>{t('Story3')}</p>
+          <br />
+          <br />
           <p>Ivan Vasilev</p>
         </div>
-
       </div>
     );
   }
 }
 
-export default withNamespaces () (MyStory);
+export default withNamespaces()(MyStory);
