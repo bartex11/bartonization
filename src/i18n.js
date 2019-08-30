d57b8572bb56fa9219cf65d9cc5639426a6d6f99
bartex11 <bartex11@yahoo.comgit-it>
2019-08-30 16:07:15 +0200

    de first

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import detector from 'i18next-browser-languagedetector';
 import backend from 'i18next-xhr-backend';
-import {reactI18nextModule} from 'react-i18next';
+import { reactI18nextModule } from 'react-i18next';
 
 import translationEN from './locales/en/translation.json';
 import translationDE from './locales/ger/translation.json';
@@ -21,13 +21,13 @@ const resources = {
 };
 
 i18n
-  .use (detector)
-  .use (backend)
-  .use (reactI18nextModule) // passes i18n down to react-i18next
-  .init ({
+  .use(detector)
+  .use(backend)
+  .use(reactI18nextModule) // passes i18n down to react-i18next
+  .init({
     resources,
-    lng: 'en',
-    fallbackLng: 'en', // use en if detected lng is not available
+    lng: 'de',
+    fallbackLng: 'de', // use en if detected lng is not available
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
