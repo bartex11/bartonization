f9f686cb206a8d365bfa346f36e00ea94ec01951
bartex11 <bartex11@yahoo.comgit-it>
2023-04-04 20:14:27 +0200

    deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bartonization/precache-manifest.b54ab44663ae58f0b757cf58c5cfe5e1.js"
+  "/bartonization/precache-manifest.733ba3f6e03d8979885c24e171a18ab7.js"
 );
 
 workbox.clientsClaim();
